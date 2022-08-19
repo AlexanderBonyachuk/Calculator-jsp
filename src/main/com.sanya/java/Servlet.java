290c0a32edb2d1e02f4deda2eb0561644987891a
Sasha40404 <70843523+Sasha40404@users.noreply.github.com>
2022-08-19 09:25:07 +0400

    Added mathematical operators

--- src/main/com.sanya/java/Servlet.java
+++ src/main/com.sanya/java/Servlet.java
@@ -11,14 +11,27 @@ public class Servlet extends HttpServlet {
     int num1 = 0;
     int num2 = 0;
     int result = 0;
+    char symbol = '+';
 
     protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         try {
             num1 = Integer.parseInt(req.getParameter("num1"));
             num2 = Integer.parseInt(req.getParameter("num2"));
+            symbol = req.getParameter("symb").charAt(0);
 
-        result = num1 + num2;
+            switch (symbol) {
+                case '+' : result = num1 + num2;
+                    break;
+                case '-' : result = num1 - num2;
+                    break;
+                case '*' : result = num1 * num2;
+                    break;
+                case '/' : result = num1 / num2;
+                    break;
+                default: break;
+            }
+
         } catch (NumberFormatException e) {
             resp.getWriter().println("Numbers not entered!");
         }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,6 +4,7 @@
     <form action="calc">
 
     Enter 1st number : <input type="text" name="num1"><br>
+    Enter symbol [+,-,*,/]: <input type="text" name="symb"><br>
     Enter 2nd number : <input type="text" name="num2"><br>
     <input name="form" type="submit" value="calculate"/>
     </form>
